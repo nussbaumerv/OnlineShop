c7d75b57ed465d11a97c3cfdb94b327cd518749e
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-10-20 13:18:03 +0200

    added

--- PHPMailer/mail.php
+++ PHPMailer/mail.php
@@ -19,7 +19,7 @@ function send_mail($to, $subject, $message)
   $mail->Username   = 'vali7799@icloud.com';
   $mail->Password   = 'ncsg-rzop-ardx-nhut';
   $mail->Port       = 587;
-  $mail->setFrom('vali7799@icloud.com', 'Edu Chat');
+  $mail->setFrom('vali7799@icloud.com', 'VHD Shop');
   $mail->addAddress($to);
 
   $mail->isHTML(true);
@@ -46,7 +46,7 @@ function send_mail($to, $subject, $message)
   </style>
 </head>
 <body style="margin:0;padding:0;">
-    <table role="presentation" style="width:100%;border-collapse:collapse;border:solid;border-spacing:0;background:#ffffff; border-color:red">
+    <table role="presentation" style="width:100%;border-collapse:collapse;border:solid;border-spacing:0;background:rgb(72, 212, 255); border-color:rgb(240, 58, 115);">
         <tr>
             <td align="left" style="padding:20px; font-size:30px;; font-weight:bold">
                 ' . $subject . '
@@ -63,9 +63,9 @@ function send_mail($to, $subject, $message)
             
         </tr>
         <tr>
-    <td align="center" style="background-color:rgb(189, 184, 184); padding:30px;">
+    <td align="center" style="background-color:rgb(240, 58, 115); padding:30px;">
     <a href="https://valentin-nussbaumer.com"><img src="https://valentin-bewerbung.com/media/Bildschirmfoto%202021-08-04%20um%2019.59.26.png" alt="" width="90" style="height:auto;display:block;" /></a>
-   <br> Edu Chat | <a style="color:black" href="https://edu-chat.me">edu-chat.me</a> | <a style="color:black" href="mailto: mail@valentin-nussbaumer.com">mail@valentin-nussbaumer.com</a>
+   <br> VHD Shop | <a style="color:black" href="https://edu-chat.me">VHD.shop</a> | <a style="color:black" href="mailto: mail@valentin-nussbaumer.com">mail@valentin-nussbaumer.com</a>
   </td>
         </tr>
     </table>
