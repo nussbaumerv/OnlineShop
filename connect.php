0a0f5879fc915de505c0dad90341d24af2829ea6
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-10-20 14:40:01 +0200

    chan

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php 
-$host_name = 'localhost';
-$user_name = 'root';
-$password = '';
-$database = 'onlineshop';
+$host_name = '109.106.246.1';
+$user_name = 'u701930346_Online';
+$password = '3yzSGbksoSih4J8q';
+$database = 'u701930346_OnlineShop';
 
 $connect = mysqli_connect($host_name, $user_name, $password, $database);
 mysqli_query($connect, "SET NAMES 'utf8'");
