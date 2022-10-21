494cfb5afc051c4593e11332190e7141e96ff017
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-10-21 08:25:01 +0200

    fixed bug

--- thanks.php
+++ thanks.php
@@ -10,30 +10,31 @@ $Validator = $_GET['key'] / $id / $id / 34;
 
 $sql = "SELECT * FROM customers WHERE id = '$id'";
 $result = mysqli_query($connect, $sql);
+if(!$result){
+    echo "Something went wrong";
+}
 $row = mysqli_fetch_assoc($result);
-
 if ($Validator == $row['validator']) {
     $products = json_decode($_SESSION['basket'], true);
 
     foreach ($products as $product) {
-        $sql = "SELECT * FROM products WHERE id = '$product'";
-        $result = mysqli_query($connect, $sql);
-        $row = mysqli_fetch_assoc($result);
-        $available = $row['available'] - 1;
+        $sql_p = "SELECT * FROM products WHERE id = '$product'";
+        $result_p = mysqli_query($connect, $sql_p);
+        $row_p = mysqli_fetch_assoc($result_p);
+        $available = $row_p['available'] - 1;
 
-        $sql_update = "UPDATE products SET available = '$available' WHERE id = '$id'";
+        $sql_update = "UPDATE products SET available = '$available' WHERE id = '$product'";
         $result_update = mysqli_query($connect, $sql_update);
     }
 
     $subject = 'Order confirmation';
-    $message = '
-                    Hi, <br>
+    $message = 'Hi, <br>
                     Thank you for your order. <br>
                     Your order of ' . $row['price'] . 'CHF will be sent to your adress in the next few days.
                     <a style="color:white;" href="https://sommernachtstraum.me/pdf.php?uid=' . $user_id_url . '">Here</a> you can download your bill as a PDF<br>
                     ';
     $to = $row['email'];
-    send_mail($to, $subject, $message);
+    //send_mail($to, $subject, $message);
 
     $sql_update = "UPDATE customers SET paid = 'Yes' WHERE id = '$id'";
     $result_update = mysqli_query($connect, $sql_update);
@@ -50,8 +51,8 @@ if ($Validator == $row['validator']) {
     <title>Danke</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap" rel="stylesheet">
 
     <style>
         body {
