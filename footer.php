ac485a08a2a78f917ee550d72dc18eb19bfa2fd2
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-10-21 13:29:10 +0200

    test fo

--- footer.php
+++ footer.php
@@ -44,6 +44,7 @@
     <a href="Shop.php">Shop</a> |
     <a href="imprint.php">Imprint</a> |
     <a href="https://github.com/nussbaumerv/OnlineShop">Github</a><br><br>
+    <?php echo $_SERVER["SCRIPT_FILENAME"];?>
 
     &copy; VHD Shop <?php echo date("Y"); ?> - All rights reserved
   </div>
