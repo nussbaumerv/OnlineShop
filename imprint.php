cccf1f8b567d576456f16541d26997d3bb6a52b6
Henrikochh <113605484+Henrikochh@users.noreply.github.com>
2022-10-21 13:30:52 +0200

    upload imprint

---
+++ imprint.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+    <link rel="stylesheet" href="about-us.css" />
+    <link rel="stylesheet" href="index.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="shortcut icon" href="https://shop.valentin-nussbaumer.com/img/1.png" type="img/vnd.microsoft.icon" />
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-12 padcol">
+                <nav class="navbar navbar-expand-lg">
+                    <img src="VHD-Logo.png" alt="" />
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse custnav" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" aria-current="" href="index.php">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="About-us.php">About us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="Kontakt.php">Kontakt</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link " href="Shop.php">Shop</a>
+                                </li>
+                            </ul>
+                            <span onclick="openNav()" class="material-symbols-outlined addBasket" style="margin-left: 455px;">
+                                shopping_cart
+                            </span>
+
+
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <h1 class="title">Imprint</h1>
+            </div>
+        </div>
+
+        <div class="row" style=" margin-top: 30px;">
+            <div class="col-12">
+                <a style="font-weight: bold;">
+                    VHD Ag
+                </a>
+            </div>
+        </div>
+        <div class="row" style="margin-top: 20px;">
+            <div class="col-12">
+                <a>
+                    Hinterbergstrasse 45
+                </a>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 10px;">
+            <div class="col-12">
+                <a>
+                    9999 VHD City
+                </a>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 10px;">
+            <div class="col-12">
+                <a>
+                    Schweiz
+                </a>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 30px;">
+            <div class="col-12">
+                <a>
+                    E-mail: VHD.info@gmail.com
+                </a>
+            </div>
+        </div>
+
+
+        <div class="row" style="margin-top: 10px;">
+            <div class="col-12">
+                <a>
+                    Telefon: +41 44 434 88 30
+                </a>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 30px;">
+            <div class="col-12">
+                <a>
+                    CEOs: Valentin, Dominic & Henri
+                </a>
+            </div>
+        </div>
+        <div class="row" style="margin-top: 30px;">
+            <div class="col-12">
+                <a>
+                    Firmennummer: CH-040.3.968.550-5
+                </a>
+            </div>
+        </div>
+        
+
+    </div>
+
+
+
+</body>
+<style>
+    @font-face {
+        font-family: Monstarize;
+        src: url("Monstarize-qZ09l.ttf");
+    }
+
+    .title {
+        color: rgb(240, 58, 115);
+        font-size: 80px;
+        font-family: Monstarize;
+        margin-top: 300px;
+    }
+</style>
+
+</html>
+
+<?php
+
+
+
+
+
+
+?>
